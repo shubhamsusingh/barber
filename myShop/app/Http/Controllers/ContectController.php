8aeb3b56303ef87aced6cd5e62fdbd44de9d5f2c
shubham singh <singhshubham68738@gmail.com>
2025-06-17 21:59:47 +0530

    working

--- myShop/app/Http/Controllers/ContectController.php
+++ myShop/app/Http/Controllers/ContectController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Order;
 class ContectController extends Controller{
 public function index(Request $request)
     {
@@ -9,8 +10,24 @@ public function index(Request $request)
     }
     public function storeContact(Request $request){
         $name = $request->input('name');
-        echo $name;
-        exit;
+        $mob=$request->input('phone');
+        $time=$request->input('time');
+        $branch=$request->input('branch');
+        $date=$request->input('date');
+        $numberOfPeople=$request->input('number');
+        $message=$request->input('message');
+        $order=new Order();
+        $order->name=$name;
+        $order->mob=$mob;
+        $order->time=$time;
+        $order->branch=$branch;
+        $order->date=$date;
+        $order->noofperson=$numberOfPeople;
+        $order->comment=$message;
+        $order->save();
+        return redirect()->back();
+        // echo $date;
+        // exit;
 
      }
 }
