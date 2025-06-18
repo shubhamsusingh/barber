e783b350bb5b76077d206f9693f9e6ce4e3eeb4f
shubham singh <singhshubham68738@gmail.com>
2025-06-19 00:00:26 +0530

    working on middleware

--- myShop/routes/web.php
+++ myShop/routes/web.php
@@ -12,12 +12,17 @@ use App\Http\Controllers\AuthController;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+
 Route::get('/',[DashboardController::class,'index'])->name('dashboard');
 Route::get('story',[StoryController::class,'index'])->name('story');
 Route::get('contect',[ContectController::class,'index'])->name('contect');
 Route::get('service',[ServicsController::class,'index'])->name('service');
 Route::get('price',[PriceController::class,'index'])->name('price');
-Route::post('contactform',[ContectController::class,'storeContact'])->name('contactform');
+// Route::post('contactform',[ContectController::class,'storeContact'])->name('contactform');
+Route::post('contactform', [ContectController::class, 'storeContact'])
+    ->middleware('auth.check')
+    ->name('contactform');
 Route::get('register',[AuthController::class,'index'])->name('register');
 Route::post('register',[AuthController::class,'register'])->name('register');
 Route::get('login',[AuthController::class,'getloginPage'])->name('login');
